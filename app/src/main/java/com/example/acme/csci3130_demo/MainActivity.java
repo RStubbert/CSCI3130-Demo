457b9ef7fed9401a281487d0b94d5d2fbd47be47
Ryan <rn917129@dal.ca>
2018-05-19 13:54:48 -0300

    Added Button and edit text field

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //Sets textView2 text to edtTxt1 when button is clicked
+    public void buttonOnClick(View v)
+    {
+        //((Button) v).setText("");
+
+        EditText edt = (EditText)findViewById(R.id.edtTxt1);
+        TextView txtView = (TextView)findViewById(R.id.textView2);
+
+        txtView.setText(edt.getText());
+
+    }
+
+
+
+
 }
